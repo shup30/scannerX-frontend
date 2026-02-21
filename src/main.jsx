b3e4aeb383ebc7bc40ebf839ba62a65e419dccdc
shup30 <shup30@gmail.com>
2026-02-21 20:38:20 +0530

    fix: register service worker and fix PWA icon purposes for Android install

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { registerSW } from 'virtual:pwa-register'
 import './i18n'
 import './index.css'
 import App from './App.jsx'
+
+// Register service worker for PWA install support
+registerSW({
+  onNeedRefresh() { },
+  onOfflineReady() { },
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
